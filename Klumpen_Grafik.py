56067dd041f9e0ee8bd23adad90ae9021e6885ab
XeEtDe <xedestuff@gmail.com>
2018-08-25 23:45:07 +0200

    Spielereinstellung weiter

--- Klumpen_Grafik.py
+++ Klumpen_Grafik.py
@@ -97,16 +97,57 @@ Kleiner_Haken = get_image("hakenklein.png")
 Kleiner_Haken_Blass = get_image("hakenkleinblass.png")
 Kreuz = get_image("kreuz.png")
 Kreuz_Blass = get_image("kreuzblass.png")
-Input_Box = textbox.TextBox((850, 320, 420, 50))
+Input_Box = textbox.TextBox((900, 320, 420, 50))
 def Print_Spieler():
-	pass
+	Spieler_Liste = Einstellungen[1]
+	Counter = len(Spieler_Liste)
+	Fertig = 0
+	while Counter > 0:
+		Spieler_Name = Spieler_Liste[Fertig]
+		Text = get_Text(Spieler_Name, 50)
+		screen.blit(Text, (185, 305 + 20*(Fertig+1)+ 50*Fertig))
+		Dings = Spieler_Hintergrund[Fertig+1]
+		Weg_Button = Dings[0]
+		if Weg_Button.Switch == False:
+			Weg_Button.Change()
+		Counter -= 1
+		Fertig += 1
+	while Fertig < 5:
+		Dings = Spieler_Hintergrund[Fertig+1]
+		Fläche = Dings[1]
+		pg.draw.rect(screen, (255, 255, 255), Fläche)
+		pg.draw.rect(screen, (0, 0, 0), Fläche, 1)
+		Weg_Button = Dings[0]
+		if Weg_Button.Switch == True:
+			Weg_Button.Change()
+		Fertig += 1
 def Spieler_Entf():
-	pass
+	for Num in Spieler_Hintergrund:
+		Dings = Spieler_Hintergrund[Num]
+		Weg_Button = Dings[0]
+		Test_Rect = pg.Rect(620, 300 + 20*Num + 50*(Num-1) + 5, Kreuz.get_width(), Kreuz.get_height())
+		if Weg_Button.Rect.contains(Test_Rect) == True:
+			if Weg_Button.Switch == True:
+				Index = Num - 1
+				Spieler_Liste = Einstellungen[1]
+				Spieler_Liste.remove(Spieler_Liste[Index])
+				Print_Spieler()
+				break
 Spieler_Hintergrund = {}
-for Num in range(1, 6):
-	Spieler_Hintergrund.update({Num:[Button(pg.Rect(400, 300 + 20*Num + 50*(Num-1) + 5, Kreuz.get_width(), Kreuz.get_height()), Spieler_Entf, Kreuz_Blass, None, Kreuz)]})
+Weg_Button_1 = Button(pg.Rect(620, 325, Kreuz.get_width(), Kreuz.get_height()), Spieler_Entf, Kreuz_Blass, None, Kreuz)
+Weg_Button_2 = Button(pg.Rect(620, 395, Kreuz.get_width(), Kreuz.get_height()), Spieler_Entf, Kreuz_Blass, None, Kreuz)
+Weg_Button_3 = Button(pg.Rect(620, 465, Kreuz.get_width(), Kreuz.get_height()), Spieler_Entf, Kreuz_Blass, None, Kreuz)
+Weg_Button_4 = Button(pg.Rect(620, 535, Kreuz.get_width(), Kreuz.get_height()), Spieler_Entf, Kreuz_Blass, None, Kreuz)
+Weg_Button_5 = Button(pg.Rect(620, 605, Kreuz.get_width(), Kreuz.get_height()), Spieler_Entf, Kreuz_Blass, None, Kreuz)
+Weg_Buttons = [Weg_Button_1, Weg_Button_2, Weg_Button_3, Weg_Button_4, Weg_Button_5]
+Counter = 1
+while Counter <= 5:
+	for B in Weg_Buttons:
+		Spieler_Hintergrund.update({Counter:[B]})
+		Counter += 1
 def Spieler_Hinzu(Name = None):
 	if Spieler_Hinzu_Button.Switch == True:
+		Input_Box.buffer = []
 		if Name == None:
 			Name = Input_Box.final
 		if Name in Einstellungen[1]:
@@ -115,8 +156,7 @@ def Spieler_Hinzu(Name = None):
 		else:
 			Einstellungen[1].append(Name)
 			Print_Spieler()
-		Spieler_Hinzu_Button.Change()
-Spieler_Hinzu_Button = Button(pg.Rect(1400, 320, Kleiner_Haken.get_width(), Kleiner_Haken.get_height()), Spieler_Hinzu, Kleiner_Haken_Blass, None, Kleiner_Haken)
+Spieler_Hinzu_Button = Button(pg.Rect(1350, 320, Kleiner_Haken.get_width(), Kleiner_Haken.get_height()), Spieler_Hinzu, Kleiner_Haken_Blass, None, Kleiner_Haken)
 
 #Regeln
 def Regeln():
@@ -153,6 +193,8 @@ def Start():
 	screen.blit(Text, (screen.get_width() / 2 - Text.get_width() / 2, 240))
 	Text = get_Text("1 bis 5", 35)
 	screen.blit(Text, (screen.get_width() / 2 - Text.get_width() / 2, 280))
+	Text = get_Text("Spielernamen eingeben", 35)
+	screen.blit(Text, (900 + (210 - Text.get_width() / 2), 380))
 	Spieler_Hinzu_Button.create_button()
 	Input_Box.process_kwargs({"command":Spieler_Hinzu})
 	Input_Box.update()
@@ -161,13 +203,12 @@ def Start():
 	Input = True
 	for Num in Spieler_Hintergrund:
 		Dings = Spieler_Hintergrund[Num]
-		Dings[0].create_button()
-		Surf = pg.Surface((420, 50))
-		Surf.fill((255, 255, 255))
-		x = 200
 		y = 300 + 20*Num + 50*(Num-1)
-		screen.blit(Surf, (x, y))
-		Spieler_Hintergrund[Num].append((x,y))
+		Fläche = pg.Rect(180, y, 420, 50)
+		pg.draw.rect(screen, (255, 255, 255), Fläche)
+		pg.draw.rect(screen, (0, 0, 0), Fläche, 1)
+		Dings[0].create_button()
+		Spieler_Hintergrund[Num].append(Fläche)
 Start_Bild = get_image("start.png")
 Text_S = get_Text("Start", 50)
 Start_Rect = pg.Rect(1000, 500, Start_Bild.get_width(), Start_Bild.get_height())
@@ -210,4 +251,11 @@ while done == False:
 	        Input_Box.get_event(event)
 	        Input_Box.update()
 	        Input_Box.draw(screen)
+	        if (not Input_Box.final == "") and (len(Einstellungen[1]) < 5):
+	        	if Spieler_Hinzu_Button.Switch == False:
+	        		Spieler_Hinzu_Button.Change()
+	        else:
+	        	if Spieler_Hinzu_Button.Switch == True:
+	        		Spieler_Hinzu_Button.Change()
+    
     pg.display.flip()
